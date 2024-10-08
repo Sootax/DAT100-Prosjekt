9d0a8adfa97414e9a6fbfa649d7267320110fede
Sootax <3jeunami82lekw@gmail.com>
2024-10-08 17:52:46 +0200

    Implementere main metoden

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -3,11 +3,12 @@ package no.hvl.dat100ptc.oppgave2;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
-
-	
 	public static void main(String[] args) {
-
-		// TODO
-		
+		GPSPoint gpsPoint1 = new GPSPoint(50, 1.0, 2.0, 5.0);
+		GPSPoint gpsPoint2 = new GPSPoint(60, 4.0, 6.0, 2.0);
+		GPSData gpsData1 = new GPSData(2);
+		gpsData1.insertGPS(gpsPoint1);
+		gpsData1.insertGPS(gpsPoint2);
+		gpsData1.print();
 	}
 }
