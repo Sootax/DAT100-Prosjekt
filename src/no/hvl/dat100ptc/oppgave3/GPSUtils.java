cf9deffe521660a6ecb05e18bd95878646070e56
h189129 <189129@stud.hvl.no>
2024-10-08 23:22:56 +0200

    Fullført oppgave 3 D
    Implementert metoden distance() og skrevet hjelpemetodene compute_a() og compute_c().

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -70,27 +70,39 @@ public class GPSUtils {
 		double d;
 		double latitude1, longitude1, latitude2, longitude2;
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		latitude1 = gpspoint1.getLatitude();
+		longitude1 = gpspoint1.getLongitude();
+		
+		latitude2 = gpspoint2.getLatitude();
+		longitude2 = gpspoint2.getLongitude();
+		
+		
+		double phi1 = toRadians(latitude1);
+		double phi2 = toRadians(latitude2);
+		double deltaphi = toRadians(latitude2 - latitude1);
+		double deltadelta = toRadians(longitude2 - longitude1);
+		
+		double a = compute_a(phi1, phi2, deltaphi, deltadelta);
+		double c = compute_c(a);
+		
+		d = R * c;
+		
+		return d;
+				
 	}
 	
 	private static double compute_a(double phi1, double phi2, double deltaphi, double deltadelta) {
-	
-		throw new UnsupportedOperationException(TODO.method());
+		double a = pow(sin(deltaphi/2), 2) + cos(phi1) * cos(phi2) * pow((sin(deltadelta/2)), 2);
 		
-		// TODO 
-
+		return a;
 	}
 
 	private static double compute_c(double a) {
-
 		
-		throw new UnsupportedOperationException(TODO.method());
+		double c = 2 * atan2(sqrt(a), sqrt(1-a));
 		
+		return c;
 		
-		// TODO 
-
 	}
 
 	
