affabf299a09f5292085846f95f79e2ffb711aeb
h189129 <189129@stud.hvl.no>
2024-10-13 20:34:31 +0200

    Uført oppgave 5 B og korrigert oppgave 5 A.

--- src/no/hvl/dat100ptc/oppgave5/ShowSpeed.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowSpeed.java
@@ -28,6 +28,7 @@ public class ShowSpeed extends EasyGraphics {
 		launch(args);
 	}
 
+	
 	public void run() {
 
 		makeWindow("Speed profile", 
@@ -37,12 +38,36 @@ public class ShowSpeed extends EasyGraphics {
 		showSpeedProfile(MARGIN + BARHEIGHT);
 	}
 	
+	
 	public void showSpeedProfile(int ybase) {
 		
-		int x = MARGIN,y;
-	
-		// TODO
-		throw new UnsupportedOperationException(TODO.method());
+		int x = MARGIN;
+		double[] speeds = gpscomputer.speeds();
+		double avgSpeed = gpscomputer.averageSpeed();
+			
+		// Tegner hver høyde i grafikk vinduet
+		for (int i = 0; i < speeds.length; i++) {
 		
+			int y = (int) speeds[i] * 5;
+			
+			drawLine(x, ybase, x, ybase - y);
+			
+			x += 2;
+				
+		}
+		
+		// Tegner rektangel
+		int xPos = MARGIN - 10;
+		int yPos = MARGIN - 20;
+		int bredde = 2 * MARGIN + 2 * speeds.length - 80;
+		int høyde = 2 * MARGIN + BARHEIGHT - 70;
+		
+		// Tegner gjennomsnittsfart som grønn linje
+		setColor(0, 0, 255);
+		drawRectangle(xPos, yPos, bredde, høyde);
+		
+		setColor(57, 255, 20);
+		drawLine(MARGIN, ybase - (int) avgSpeed * 5, x, ybase - (int) avgSpeed * 5);
+			
 	}
 }
